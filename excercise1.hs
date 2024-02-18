ba401ae76f0ade932cae47b00a993283b13ec7a3
Harsh <harshrawat096@gmail.com>
2024-02-18 20:59:58 +0530

    More excercise!

--- excercise1.hs
+++ excercise1.hs
@@ -20,5 +20,19 @@ rev = reverse mylist
 rev1 = [mylist !! (x-1) | x<-[length mylist,length mylist-1..1]]
 
 -- Q6 Find out whether a list is a palindrome.
-pal = sum([1 |x<-[length mylist-1,length mylist-2..0],y<-[0..length mylist-1], mylist !! x == mylist !! y])==length mylist
+-- pal = sum([1 |x<-[length mylist-1,length mylist-2..0],y<-[0..length mylist-1], mylist !! x == mylist !! y])==length mylist
 pali = mylist == reverse mylist
+
+pal = sum([ 1 | x<-[0..length mylist-1], (mylist !! x )== mylist !! (length mylist-x-1 )])==length mylist
+
+
+-- Q7  Flatten a nested list structure.
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+flat = [x|xs<-arr,x<-xs]
+flatrev = [x|xs<-reverse arr,x<-reverse xs]
+
+-- Q8 Eliminate consecutive duplicates of list elements
+arr1 = ['a','a','a','b','c','a','b','c','d','d','d','d','e']
+-- dup = [arr1 !! x | x<-[0..length arr1-2],arr1 !! x /= arr1 !! (x+1) ]
+
+
